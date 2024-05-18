bc07bf4d82ec126c51f6915d4ad63fb5196ac679
Rafaela Soares <160733605+rafaelassoaress@users.noreply.github.com>
2024-05-18 11:09:30 -0300

    Aula10
    Finalização de assunto CSS e inicio de JS.

---
+++ js/login.js
@@ -0,0 +1,22 @@
+// document.getElementById("formLogin").addEventListener('submit',
+// function(event){
+// alert('Olá Mundo')
+// })
+
+
+function ola(){
+
+    let email = document.getElementById("email_form").value
+    let senha = document.getElementById("senha_form").value
+
+    console.log(email,senha)
+    if(email==""||senha==""){
+    alert('Login Recusado')
+
+    }else{
+        alert('Login Aprovado')
+    }
+        
+}
+
+console.log("teste",123, 10.99, true)
